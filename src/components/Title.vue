be4dd073590a1d747cdf2454344b79a2c2b17a42
XanderGale <alessandro.galeazzi@icloud.com>
2021-12-17 18:55:59 +0100

    completed (missing jumbo)

---
+++ src/components/Title.vue
@@ -0,0 +1,15 @@
+<template>
+    <h3 class="title">La mia prima app con Vue CLI!</h3>
+</template>
+
+<script>
+    export default{
+        name: 'Title',
+    }
+</script>
+
+<style scoped>
+    .title{
+        color: darkslategray;
+    }
+</style>
