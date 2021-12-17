33f4cebf28898c17778755ea3bf1bd4aa3a7ef45
XanderGale <alessandro.galeazzi@icloud.com>
2021-12-17 19:12:47 +0100

    small changes

--- src/components/Jumbo.vue
+++ src/components/Jumbo.vue
@@ -13,6 +13,9 @@ export default {
 <style scoped>
     .jumbo{
         background-image: url('../assets/img/batman-banner.jpeg');
+        background-repeat: no-repeat;
+        background-size: cover;
+        background-position: center;
 
         width: 100%;
         height: 80vh;
